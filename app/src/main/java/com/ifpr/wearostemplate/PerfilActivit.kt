fb630d46a63739fb8fbd8c52d311f6538b6d343b
Henriquegnatta <henriquegnatta@gmail.com>
2026-08-11 09:43:49 -0300

    Segunda tela
    segunda tela criada so resta criar o botao para salva no banco de dados

--- app/src/main/java/com/ifpr/wearostemplate/PerfilActivit.kt
+++ app/src/main/java/com/ifpr/wearostemplate/PerfilActivit.kt
@@ -1,19 +1,39 @@
 package com.ifpr.wearostemplate
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PerfilActivit : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_perfil)
+
+        val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            val systemBars = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars()
+            )
+
+            v.setPadding(
+                systemBars.left,
+                systemBars.top,
+                systemBars.right,
+                systemBars.bottom
+            )
+
             insets
         }
     }
--- app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
+++ app/src/main/java/com/ifpr/wearostemplate/presentation/MainActivity.kt
@@ -1,20 +1,17 @@
-/* While this template provides a good starting point for using Wear Compose, you can always
- * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter to find the
- * most up to date changes to the libraries and their usages.
- */
-
 package com.ifpr.wearostemplate.presentation
 
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ifpr.wearostemplate.R
-import com.ifpr.wearostemplate.presentation.theme.corrida
-import android.content.Intent
-import android.widget.Button
+import com.ifpr.wearostemplate.PerfilActivit
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
@@ -22,12 +19,12 @@ class MainActivity : ComponentActivity() {
         setTheme(android.R.style.Theme_DeviceDefault)
 
         setContentView(R.layout.activity_main)
+
         val btncorrida = findViewById<Button>(R.id.btnCorre)
 
         btncorrida.setOnClickListener {
-            val intent = Intent(this, corrida::class.java)
+            val intent = Intent(this, PerfilActivit::class.java)
             startActivity(intent)
         }
     }
 }
-
